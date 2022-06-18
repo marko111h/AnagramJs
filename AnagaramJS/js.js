2439bcceddf4555759eaaf0bfeb5186eb4065549
marko111h <marko.dj111@gmail.com>
2022-06-18 14:48:44 +0200

    Anagram Fixed

--- AnagaramJS/js.js
+++ AnagaramJS/js.js
@@ -1,62 +1,32 @@
-const form = document.getElementById('foruum')
+const form = document.getElementById("foruum");
 function handleForm(event) {
-  event.preventDefault()
+  event.preventDefault();
 }
-form.addEventListener('submit', handleForm)
-const rez = 0
+form.addEventListener("submit", handleForm);
+
 function myFunction() {
-  const x = document.getElementById('prvaRec').value
-  const y = document.getElementById('drugaRec').value
+  const firstWord = window.document.getElementById("prvaRec").value;
+  const secoundWord = window.document.getElementById("drugaRec").value;
+  const api_url = `http://localhost:20010/anagram/:${firstWord}/:${secoundWord}`;
 
-  const data = { username: 'example' }
-  const api_url = 'http://localhost:20010/anagram/:value1/:value2'
-  //console.log(fetch('http://localhost:20010/anagram'))
-
-  fetch('http://example.com/movies.json')
+  fetch(api_url, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+    },
+  })
     .then((response) => {
-      console.log(response)
-      return response.json()
+      console.log(response);
+      return response.text();
     })
-    .then((data) => {
-      // Work with JSON data here
-      console.log(data)
+    .then((text) => {
+      console.log(text);
+      document.getElementById("daLiJeAnagaram").innerHTML = text;
     })
     .catch((err) => {
-      // Do something for an error here
-      console.log('Error Reading data ' + err)
-    })
-  /*async function getapi(url) {
-    const response = await fetch(url)
-
-    var data = await response.json()
-    console.log(data)
-  }
-  getapi(api_url)
-*/
-  /* fetch('/localhost:20010/apia/:value1/:value2', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((error) => {
-      console.error('Error', error)
-    })
-}
-
-/*const data = { asd, asd }
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  }
-  fetch('/api', options)
-}*/
+      console.log("Error Reading data " + err);
+      document.getElementById("daLiJeAnagaram").innerHTML =
+        "Error reading data";
+    });
 }
