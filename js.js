3ec15eb57c10fd3eca0fcba45592623ac83a8210
marko111h <marko.dj111@gmail.com>
2022-08-10 00:45:24 +0200

    Fixed some stuff

---
+++ js.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("foruum");
+function handleForm(event) {
+  event.preventDefault();
+  myFunction();
+}
+form.addEventListener("submit", handleForm);
+
+function myFunction() {
+  const firstWord = document.getElementById("prvaRec").value;
+  const secoundWord = document.getElementById("drugaRec").value;
+  const api_url = `http://localhost:20010/anagram/:${firstWord}/:${secoundWord}`;
+
+  fetch(api_url, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      return response.text();
+    })
+    .then((text) => {
+      console.log(text);
+      document.getElementById("daLiJeAnagaram").innerHTML = text;
+    })
+    .catch((err) => {
+      console.log("Error Reading data " + err);
+    });
+}
